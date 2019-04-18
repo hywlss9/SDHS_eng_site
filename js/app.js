16a7d197f5228719ac4824bd59b4a99a2221d1d5
hywlss9 <hywlss9@gmail.com>
2019-04-18 20:10:11 +0900

    header 익스 수정 해야됨

--- js/app.js
+++ js/app.js
@@ -1,22 +1,38 @@
 function event(){
-   	$(document)
-   	.on("mouseover", ".side-menu-list>li", function(){
-      	$(".active").removeClass("active");
-      	$(this).find("ul").addClass("active");
-   	})
-   	.on("mouseout", ".side-menu-list>li", function(){
-      	$(this).find("ul").removeClass("active");
-   	})
-   	.on("scroll", function(){
-      	let h = $("aside>img").height();
-      	let dh = $(document).height();
-      	let wh = $(window).height();
-      	let st = $(document).scrollTop();
-		let top= ((h-wh)*st)/(dh-wh);
-      	$("aside>img").css({"top": -top+"px"});
-   	})
+    $(document)
+    // .on("mousemove", function(e){
+    // 	if($(e.target).parent("ul").attr("class") == "side-menu-list"){
+    // 		$(".active").removeClass("active");
+    //     	$(this).find("ul").addClass("active");
+    // 	}else if($(e.target).parents("ul").attr("class") == "side-menu-list"){
+    // 		$(this).addClass("active");
+    // 	}else{
+    // 		$(document).find("ul").removeClass("active");
+    // 	}
+    // })
+    .on("mouseover", ".side-menu-list>li ul", function(e){
+        $(this).addClass("active");
+    })
+    .on("mouseover", ".side-menu-list>li", function(){
+        $(".active").removeClass("active");
+        $(this).find("ul").addClass("active");
+    })
+    .on("mouseout", ".side-menu-list>li", function(e){
+        $(this).find("ul").removeClass("active");
+    })
+    .on("scroll", function(){
+        scroll();
+    })
 }
-
+function scroll(){
+   	var h = $("aside>img").height();
+    var dh = $(document).height();
+    var wh = $(window).height();
+    var st = $(document).scrollTop();
+   	var top= ((h-wh)*st)/(dh-wh);
+    $("aside>img").css({"top": -top+"px"});
+}
 window.onload = function(){
-   	event();
+    event();
+    scroll();
 }
