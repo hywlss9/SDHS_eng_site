11a5c0b2327955d62a16216ff05b753ded5a15a8
hywlss9 <hywlss9@gmail.com>
2019-04-19 10:07:07 +0900

    sub 완료

--- js/app.js
+++ js/app.js
@@ -1,38 +1,62 @@
-function event(){
+var broserch = function(){ //브라우저 체크
+    var word;
+    var version = "N/A";
+    var agent = navigator.userAgent.toLowerCase();
+    var name = navigator.appName;
+
+    // IE old version ( IE 10 or Lower )
+    if ( name == "Microsoft Internet Explorer" ) word = "msie ";
+
+    else {
+        // IE 11
+        if ( agent.search("trident") > -1 ) word = "trident/.*rv:";
+
+        // IE 12  ( Microsoft Edge )
+        else if ( agent.search("edge/") > -1 ) word = "edge/";
+    }
+
+    var reg = new RegExp( word + "([0-9]{1,})(\\.{0,}[0-9]{0,1})" );
+    if (  reg.exec( agent ) != null  )
+        version = RegExp.$1 + RegExp.$2;
+
+    return version;
+};
+if(broserch() == "9.0" || broserch() == "8.0" || broserch() == "7.0" || broserch() == "6.0" || broserch() == "5.0"){
+	alert("You are using an older browser. Please use the latest browser.");
+	window.open('','_self').close();
+}else{
+	window.onload = function(){
+	    event();
+	    scroll();
+	}
+}
+
+function event(){ //event
     $(document)
-    // .on("mousemove", function(e){
-    // 	if($(e.target).parent("ul").attr("class") == "side-menu-list"){
-    // 		$(".active").removeClass("active");
-    //     	$(this).find("ul").addClass("active");
-    // 	}else if($(e.target).parents("ul").attr("class") == "side-menu-list"){
-    // 		$(this).addClass("active");
-    // 	}else{
-    // 		$(document).find("ul").removeClass("active");
-    // 	}
-    // })
-    .on("mouseover", ".side-menu-list>li ul", function(e){
-        $(this).addClass("active");
-    })
-    .on("mouseover", ".side-menu-list>li", function(){
+    .on("mouseenter", ".side-menu-list>li", function(){
         $(".active").removeClass("active");
         $(this).find("ul").addClass("active");
     })
-    .on("mouseout", ".side-menu-list>li", function(e){
+    .on("mouseleave", ".side-menu-list>li", function(e){
         $(this).find("ul").removeClass("active");
     })
     .on("scroll", function(){
         scroll();
     })
+    .on("mouseenter", ".side-menu-list>li>ul>li", function(){
+    	$(this).css({"background":"#3aabdc"});
+    	$(this).find("a").css({"color":"#fff"});
+    })
+    .on("mouseleave", ".side-menu-list>li>ul>li", function(){
+    	$(this).css({"background":"none"});
+    	$(this).find("a").css({"color":"#000"});
+    })
 }
-function scroll(){
+function scroll(){ //sidemenu scroll event
    	var h = $("aside>img").height();
     var dh = $(document).height();
     var wh = $(window).height();
     var st = $(document).scrollTop();
    	var top= ((h-wh)*st)/(dh-wh);
     $("aside>img").css({"top": -top+"px"});
-}
-window.onload = function(){
-    event();
-    scroll();
 }
